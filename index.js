c0a1b6ae80fab8a6dcae221570a5add64a2ec02c
niole <niolenelson@gmail.com>
2023-05-21 17:36:14 -0700

    Add ui

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-import * as wasm from "hello-wasm-pack";
+import {Game} from "wasm-game-of-life";
 
-wasm.greet();
+const gameDiv = document.createElement('div');
+
+document.body.append(gameDiv);
+
+const game = Game.new();
+render();
+
+setTimeout(() => {
+  game.on_tick()
+  render();
+}, 1000);
+
+function render() {
+  gameDiv.innerHTML = game.render();
+}
