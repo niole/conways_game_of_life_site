918735d6bce8b75755813aeef32e85b1badc95fb
niole <niolenelson@gmail.com>
2023-05-27 17:05:52 -0700

    Make a board

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import {Game} from "wasm-game-of-life";
 const gameDiv = document.createElement('div');
 
 document.body.append(gameDiv);
-
-const game = Game.new();
+const board = [0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0];
+const game = Game.new_board(board);
 render();
 
-setTimeout(() => {
+setInterval(() => {
   game.on_tick()
   render();
 }, 1000);
